6f66e5de6fba99f440ab127732705b6cc2686f10
Joseph Isbell <shadowwolf347@comcast.net>
2018-07-17 13:59:47 -0600

    Adjusted code based on example taught

--- challenge.php
+++ challenge.php
@@ -1,19 +1,34 @@
 <?php
-function assocArray(){
-	$array = array(
-				["name" => "Jean-Luc Picard",
-		"		age"  => 42
-				],
-				["name" => "Mr. Spock",
-			"		age"  =>  28
-				]
-	);
 
-	foreach($array as $key => $value) {
-		print_r($value['name']);
-		print_r($value['age'] / count($value));
+$paramArray = [
+	["name" => "Goku", "age" => 40],
+	["name" => "Luffy", "age" => 20],
+	["name" => "Zoro", "age" => 22],
+	["name" => "Ichigo", "age" => 19],
+];
 
-	}
-};
+/**
+ *A function that takes an associative array of people with names and ages and returns an average age.
+ *
+ *@param array $personsArray an array of people
+ *@return int $averageAge average age of people in the array
+ **/
 
-assocArray();
+// the array in the function will only allow the function to pass in an array and nothing else. It will return an integer
+function averageAge(array $personsArray) : float {
+	$totalAge = 0;
+	// feeds in the personsArray and returns a person(key = in this case age) out of it each time it loops
+	foreach($personsArray as $person) {
+		//echo only works on a string     //PHP_EOL means end of line and creates a new line when outputing the values from the script
+		echo $person['name'] ."'s " . "age is " . $person["age"] . PHP_EOL;
+		//Adds the persons age value to the $totalAge variable each time the loop runs and stores it back in $totalAge
+		$totalAge += $person["age"];
+		}
+
+	//make sure $totalAge is the correct number
+	echo "The total age is " . $totalAge . PHP_EOL;
+
+	return ($totalAge / count($personsArray));
+}
+
+echo "The average age is " . averageAge($paramArray) .PHP_EOL;
